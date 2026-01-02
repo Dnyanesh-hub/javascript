d360d53121b01cc632529f3449b4cea15fd3cfd2
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-01-02 13:46:15 +0530

    generating otp with the help of time

--- math.js
+++ math.js
@@ -6,11 +6,13 @@ let num2=(Math.floor(Math.random()*(99999-10000)+1))+10000;
 console.log(num2);
 //write and random math function 
  function random(){
-    let num=25987**0.5;// this is the seed or input // here the value is remaming constst we need variable values
+    let now=new Date();
+    let num =now.getMilliseconds();
+    //let num=25987**0.5;// this is the seed or input // here the value is remaming constst we need variable values
     num= num**3;
     num/=10000;
     let decimal='.'+num.toString().split(".")[1];
-    console.log(decimal);
+    return Number(decimal);
     //formula for the genrating otp using math .random function
     //math.floor(math.randm()*(max-min)+1)+min
     
@@ -18,3 +20,8 @@ console.log(num2);
 
 }
 random();
+// generate otp 
+function generate(){
+    console.log(Math.floor(random()*(9999-1000)+1))+1000;
+}
+generate();
