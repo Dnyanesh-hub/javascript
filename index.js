8d2da9fff0c75cc29f5d630820240086dc12807d
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-01-02 13:06:33 +0530

    Added math practice JavaScript files

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log("hello dany")
+let number=20;
+console.log(number);
+let d="dany";
+console.log(d);
+let obj={
+   name:"shivani",
+   age1:19,
+   name2:"dany",
+   age2:20,
+
+    
+
+};
+console.log(obj.name,obj.age1,obj.name2,obj.age2);
+function add(){
+    console.log("this is my first java script function");
+}
+add();
+let a=function add2(a,b){
+    console.log("addition of two numbers:",a+b);
+
+}
+a(20,50);
---
+++ math.js
@@ -0,0 +1,18 @@
+// let num=(Math.floor(Math.random()*10))+1;
+// console.log(num);
+//otp generation using math.random but it is not safe cuz it take time as seed or input to generate some xyz otp 
+//generate 5 digit otp 100000-99999
+let num2=(Math.floor(Math.random()*(99999-10000)+1))+10000;
+console.log(num2);
+//write and random math function 
+ function random(){
+    let num=25987**0.5;// this is the seed or input // here the value is remaming constst we need variable values
+    num= num**3;
+    num/=10000;
+    let decimal='.'+num.toString().split(".")[1];
+    console.log(decimal);
+    
+    
+
+}
+random();
