c27deaf217bad880c6fc5a9330ba2706b7908060
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-01-08 10:30:30 +0530

    added how to resolve queries parallelly

--- asyncawait.js
+++ asyncawait.js
@@ -14,7 +14,11 @@
 // const data=await response.json();
 // console.log(data);
 async function github() {
+    try{
     const response=await fetch("https://api.github.com/users");
+    if(!response.ok){
+        throw new Error("Data is not present");
+    }
     const data=await response.json();
     const parent =document.getElementById("first");
     // console.log(data);
@@ -32,9 +36,26 @@ async function github() {
         parent.append(element);
 
     }
+}
+catch(error){
+    console.log("Error:",error);
+
+
+}
 
     
 }
 github();
 
+async function userdetails() {
+    // const comment=await fetch ("userComment");
+    // const photos=await fetch ("userphoto");
+    // const chat=await fetch("chat");
+    // above three tasks are independ of each other they are making our code slow
+    // and untill  the execution of the first response second will not start running 
+    //so to solve this problem we are running this parralely like following
+   const[comment,photos,chat] =await Promise.all([fetch("userComment"),fetch("userphoto"),fetch("chat")]);
+
+    
+}
 
